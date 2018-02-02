ea81c382c33dcc909b6d36559b22387b936d5ffb
Teerawut101 <Teerawut.j@outlook.com>
2018-02-02 13:01:19 +0700

    Update motobit.ts

--- motobit.ts
+++ motobit.ts
@@ -23,7 +23,7 @@ enum MotorPower {
  * Functions to operate the moto:bit
  */
 //% color=#f44242 icon="\uf1b9" block="moto:bit"
-namespace motobit {
+namespace ibit {
 	/**
 	 * Sets the speed and direction of either the left motor or the right motor.
      * @param motor the motor to act on
@@ -51,6 +51,26 @@ namespace motobit {
         pins.i2cWriteNumber(89, (motor + pwr), NumberFormat.Int16BE)
     }
 
+    //% blockId="motobit_setMotor" block="move %motor_number motor|motor %path|at %speed|%"
+    //% speed.min=0 speed.max=100
+    //% weight=80
+    export function Motor(motor: Motor, direction: MotorDirection, speed: number): void {
+        let pwr = 0
+        speed = Math.abs(speed)
+        if (speed > 100) {
+            speed = 100
+        }
+
+        if (direction == MotorDirection.Forward) {
+            pwr = pins.map(speed, 0, 100, 0, 127)
+            pwr = 128 + pwr
+        }
+        else {
+            pwr = pins.map(speed, 0, 100, 127, 0)
+        }
+
+        pins.i2cWriteNumber(89, (motor + pwr), NumberFormat.Int16BE)
+    }
 	/**
 	 * Turns the motors on or off.
 	 */
@@ -62,9 +82,11 @@ namespace motobit {
 
     //% weight=90
     //% blockId="pinON" block="PinOn %command"
-    export function PinOn(command: MotorPower): void {
+    export function moto(command: MotorPower): void {
         pins.i2cWriteNumber(89, command, NumberFormat.Int16BE)
     }
+
+
 	/**
 	 * Changes the polarity of the selected motor.
 	 * i.e. Forward -> Backward and Backward -> Forward
